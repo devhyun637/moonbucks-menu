d38a0966bdc80918ec670f1895170d0ae995cb62
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-10-12 19:45:06 +0900

    feat: 메뉴 수정 및 삭제 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -29,10 +29,45 @@ const moonBucksApp = () => {
 
   function renderMenuList(items) {
     $menuList.innerHTML = items.map(renderMenuItem).join("");
+
+    addEditEvent(items);
+    addDeleteEvent(items);
   }
 
   function renderMenuItem(item) {
     return menuItem(item);
+  }
+
+  function addEditEvent(items) {
+    const $editButtons = document.querySelectorAll(".menu-edit-button");
+
+    [...$editButtons].forEach((button, index) =>
+      button.addEventListener("click", () => onEditButton(items, index))
+    );
+  }
+
+  function addDeleteEvent(items) {
+    const $deleteButtons = document.querySelectorAll(".menu-remove-button");
+
+    [...$deleteButtons].forEach((button, index) =>
+      button.addEventListener("click", () => deleteButton(items, index))
+    );
+  }
+
+  function onEditButton(items, index) {
+    const editName = window.prompt(
+      `${index + 1}번째 메뉴 ${items[index]}의 이름을 수정해주세요`
+    );
+
+    items.splice(index, 1, editName);
+    updatedView(items);
+  }
+
+  function deleteButton(items, index) {
+    window.confirm("정말로 삭제하시겠습니까?");
+
+    items.splice(index, 1);
+    updatedView(items);
   }
 };
 
